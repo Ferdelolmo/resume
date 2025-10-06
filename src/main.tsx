a4e9e9fc056cebf64f831008618adc95b293f4ea
Ferdelolmo <fernandodelolmovelayos@gmail.com>
2025-10-06 17:03:09 +0200

    adding faro

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,33 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import { matchRoutes } from 'react-router-dom';
+import { initializeFaro, createReactRouterV6DataOptions, ReactIntegration, getWebInstrumentations, } from '@grafana/faro-react';
+import { TracingInstrumentation } from '@grafana/faro-web-tracing';
+
+initializeFaro({
+  url: 'https://faro-collector-prod-us-central-0.grafana.net/collect/104050330065aa45e93aac7ec2b44cbe',
+  app: {
+    name: 'my website',
+    version: '1.0.0',
+    environment: 'production'
+  },
+  
+  instrumentations: [
+    // Mandatory, omits default instrumentations otherwise.
+    ...getWebInstrumentations(),
+
+    // Tracing package to get end-to-end visibility for HTTP requests.
+    new TracingInstrumentation(),
+
+    // React integration for React applications.
+    new ReactIntegration({
+      router: createReactRouterV6DataOptions({
+        matchRoutes,
+      }),
+    }),
+  ],
+});
+
 
 createRoot(document.getElementById("root")!).render(<App />);
